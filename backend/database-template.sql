600bb7ff750afc1ca5abd6c6aa5b41ed9aa266d5
cassiopeelaurie <cassiopee.laurie@gmail.com>
2024-04-08 21:21:39 +0200

    database file tables

--- backend/database-template.sql
+++ backend/database-template.sql
@@ -1,6 +1,42 @@
 -- Active: 1699869369987@@127.0.0.1@3306
 CREATE DATABASE portfolio DEFAULT CHARACTER SET = 'utf8mb4';
 
-USE DATABASE portfolio;
+USE portfolio;
 
-CREATE TABLE 
+CREATE TABLE skill (
+    id int PRIMARY KEY AUTO_INCREMENT, skills VARCHAR(100)
+);
+
+DROP TABLE skill;
+
+INSERT INTO
+    skill (skills)
+VALUES ('html'),
+    ('css'),
+    ('javascript'),
+    ('react'),
+    ('node.js'),
+    ('express');
+
+CREATE TABLE project (
+    id int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), description VARCHAR(255)
+);
+
+DROP TABLE project;
+
+INSERT INTO
+    project (name, description)
+VALUES (
+        'Wild Eats', 'Première description de projet'
+    ),
+    (
+        'GreenPoint', 'Deuxième description de projet'
+    ),
+    (
+        'Tech Care', 'Troisième description de projet'
+    ),
+    (
+        'Externatic', 'Quatrième description de projet'
+    );
+
+create TABLE project_skill
