3a7be4be7259c6fdeb28d657536c20738cc2e77c
cassiopeelaurie <cassiopee.laurie@gmail.com>
2024-04-09 11:01:06 +0200

    added all tables needed for db

--- backend/database-template.sql
+++ backend/database-template.sql
@@ -39,4 +39,71 @@ VALUES (
         'Externatic', 'Quatrième description de projet'
     );
 
-create TABLE project_skill
+create TABLE project_skill (
+    project_id int, skill_id int, FOREIGN KEY (project_id) REFERENCES project (id), FOREIGN KEY (skill_id) REFERENCES skill (id)
+);
+
+DROP TABLE project_skill;
+
+INSERT INTO
+    project_skill (project_id, skill_id)
+VALUES (1, 1),
+    (1, 2),
+    (1, 3),
+    (2, 1),
+    (2, 2),
+    (2, 3),
+    (2, 4),
+    (3, 1),
+    (3, 2),
+    (3, 3),
+    (3, 4),
+    (3, 5),
+    (3, 6),
+    (4, 1),
+    (4, 2),
+    (4, 3),
+    (4, 4),
+    (4, 5),
+    (4, 6);
+
+create TABLE tool (
+    id int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100)
+);
+
+DROP TABLE tool;
+
+INSERT INTO
+    tool (name)
+VALUES ('Trello'),
+    ('GitHub'),
+    ('Figma'),
+    ('Postman'),
+    ('DBeaver')
+
+create TABLE hard_skill (
+    id int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100)
+);
+
+DROP TABLE tool;
+
+INSERT INTO
+    hard_skill (name)
+VALUES ('HTML5'),
+    ('CSS3'),
+    ('JavaScript'),
+    ('Node.JS'),
+    ('React'),
+    ('Express')
+
+create TABLE formation (
+    id int PRIMARY KEY AUTO_INCREMENT, name VARCHAR(100), description VARCHAR(255)
+);
+
+DROP TABLE tool;
+
+INSERT INTO
+    formation (name, description)
+VALUES (
+        'Wild Code School', "Ceci est une description de ma formation, avec le type de diplôme obtenue et ce que j'ai appris et les dates"
+    )
