c2cbe0a8247b15789ba42b8e887b6cbe666d750c
cassiopeelaurie <cassiopee.laurie@gmail.com>
2024-04-12 15:50:24 +0200

    installer axios and started fetch in home page

--- frontend/src/components/others/SelectedWork.jsx
+++ frontend/src/components/others/SelectedWork.jsx
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
+import axios from "axios";
 import "./selectedWork.css";
 import ViewButton from "../buttons/ViewButton";
 import Learn from "../../assets/learn-home.jpg";
 
 export default function SelectedWork() {
+  const [projectSkills, setProjectSkills] = useState(null);
+
+  useEffect(() => {
+    // Définissez une fonction pour récupérer les données du projet
+    const fetchProjectSkills = async () => {
+      try {
+        const response = await axios.get("/projects-skills/:id");
+        setProjectSkills(response.data); // Mettez à jour l'état avec les données de la réponse
+      } catch (error) {
+        console.error("Error fetching project skills:", error);
+      }
+    };
+
+    fetchProjectSkills(); // Appelez la fonction pour récupérer les données du projet
+  }, []);
+
   return (
     <div className="selected-work-container">
       <h1 className="title-about-project">Selected Work</h1>
@@ -12,7 +29,7 @@ export default function SelectedWork() {
           <img className="img-project" src={Learn} alt="project" />
         </div>
         <div>
-          <h3 className="title-selected-project">GreenPoint</h3>
+          <h3 className="title-selected-project">{projectData.name}</h3>
           <p className="description-selected-project">
             Vous êtes soucieux de l'environnement ? Vous souhaitez connaître
             votre empreinte carbone et souhaitez vous améliorer ? GreenPoint est
