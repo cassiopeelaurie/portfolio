164c4c3916e35d954213c879e7c54f1b2de11e8c
cassiopeelaurie <cassiopee.laurie@gmail.com>
2024-04-17 22:08:10 +0200

    continue css

--- frontend/src/components/cards/AboutCards.jsx
+++ frontend/src/components/cards/AboutCards.jsx
@@ -4,8 +4,16 @@ import "./aboutCards.css";
 export default function AboutCards({ NameCards, className, textCards }) {
   return (
     <div className={`about-cards-container ${className}`}>
-      <h1 className="name-cards">{NameCards}</h1>
-      <div>{textCards}</div>
+      <div className="name-cards-container">
+        <h1 className="name-cards">{NameCards}</h1>
+      </div>
+      <div className="text-cards-container">
+        {typeof textCards === "string" ? (
+          <p className="text-cards">{textCards}</p>
+        ) : (
+          <div className="text-cards">{textCards}</div>
+        )}
+      </div>
     </div>
   );
 }
